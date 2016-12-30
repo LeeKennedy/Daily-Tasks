54a8362ed8e544f1d3ae109a33ac1dd4c1c23a4f
LeeKennedy <mail@leekennedy.id.au>
2016-12-30 13:09:46 +1100

    Playing with structure

--- scripts/Two.Sample.Comparison.R
+++ scripts/Two.Sample.Comparison.R
@@ -9,14 +9,12 @@ library(tidyr)
 library(ggplot2)
 
 # Data Input -------------------------------------------------------------
-data.in <- read_excel("C:/Users/leekennedy/Desktop/twotimes.xlsx", 
-                      col_types = c("numeric", "text", "text", 
-                                    "text", "text", "numeric", "text", 
-                                    "numeric"))
+data.in <- read_excel("C:/Users/leekennedy/Desktop/Nestle.xlsx")
 
 # Data Cleaning ----------------------------------------------------------
 
-analytes <- c("Calcium", "Copper", "Iron", "Magnesium", "Manganese", "Potassium", "Sodium", "Zinc", "Ash (@ 550°C)")
+analytes <- c("Calcium", "Copper", "Iron", "Magnesium", "Manganese", "Potassium", "Sodium", "Zinc", "Ash (@ 550?C)")
+#analytes <- unique(data.in$REPORTED_NAME)
 
 data.in$SAMPLE_NUMBER <- paste(data.in$SAMPLE_NUMBER,"_",data.in$REPLICATE_COUNT, sep="")
 
@@ -28,7 +26,16 @@ data.2 <- data.2 %>%
   filter(REPORTED_NAME %in% analytes)
 
 data.3 <- spread(data.2, SAMPLE_NUMBER, ENTRY)
+
+colnames(data.3)[3] <- "Test"
+colnames(data.3)[4] <- "Retest"
+
+data.3 <- data.3 %>% 
+  mutate(Ratio = round(Retest/Test,2))
+
 print(tbl_df(data.3), n=40)
+
+write_csv(data.3, "C:/Users/leekennedy/Desktop/Nestle_summary.csv")
 
 # Visualising Data ----------------------------------------------------
 
