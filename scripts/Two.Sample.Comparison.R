8248444686dfd068219c6fc6d591ddba6c13c700
LeeKennedy <mail@leekennedy.id.au>
2016-12-31 22:05:20 +1100

    Smoothing rough edges

--- scripts/Two.Sample.Comparison.R
+++ scripts/Two.Sample.Comparison.R
@@ -9,33 +9,53 @@ library(tidyr)
 library(ggplot2)
 
 # Data Input -------------------------------------------------------------
-data.in <- read_excel("C:/Users/leekennedy/Desktop/Nestle.xlsx")
+data.in <- read_csv("~/Desktop/Two_Samples.csv")
 
 # Data Cleaning ----------------------------------------------------------
 
-analytes <- c("Calcium", "Copper", "Iron", "Magnesium", "Manganese", "Potassium", "Sodium", "Zinc", "Ash (@ 550?C)")
-#analytes <- unique(data.in$REPORTED_NAME)
+#analytes <- c("Calcium", "Copper", "Iron", "Magnesium", "Manganese", "Potassium", "Sodium", "Zinc", "Ash (@ 550?C)")
+analytes <- unique(data.in$REPORTED_NAME)
 
+# Save sample names ------------------------------------------------------
+samples <- unique(data.in$SAMPLE_NUMBER)
+
+# Identify replicates, if present ----------------------------------------
 data.in$SAMPLE_NUMBER <- paste(data.in$SAMPLE_NUMBER,"_",data.in$REPLICATE_COUNT, sep="")
 
 data.2 <- data.in[,c(1,3,5,6)]
+data.2$ENTRY <- as.numeric(data.2$ENTRY)
 
 data.2$ENTRY <- round(data.2$ENTRY,3)
 
+# Use analytes list to select elements (Optional) ------------------------
 data.2 <- data.2 %>%
   filter(REPORTED_NAME %in% analytes)
 
+data.2 <- data.2 %>% 
+  arrange(REPORTED_NAME) %>% 
+  group_by(SAMPLE_NUMBER) %>% 
+  mutate(ID = seq_along(SAMPLE_NUMBER))
+
 data.3 <- spread(data.2, SAMPLE_NUMBER, ENTRY)
 
-colnames(data.3)[3] <- "Test"
-colnames(data.3)[4] <- "Retest"
+colnames(data.3)[4] <- "Test"
+colnames(data.3)[5] <- "Retest"
 
 data.3 <- data.3 %>% 
   mutate(Ratio = round(Retest/Test,2))
 
-print(tbl_df(data.3), n=40)
+print(tbl_df(data.3))
 
-write_csv(data.3, "C:/Users/leekennedy/Desktop/Nestle_summary.csv")
+write_csv(data.3, "summary.csv")
 
 # Visualising Data ----------------------------------------------------
 
+plot_data <- ggplot(data.3, aes(x = Test, y = Retest)) +
+  geom_point(size=4, shape = 21, colour = 'black', fill = 'cornflowerblue') +
+  geom_abline(slope = 1, intercept = 0, lty = 2, colour = 'red') +
+  theme_bw() +
+  theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
+        axis.line = element_line(size = 0.7, color = "black"), 
+        text = element_text(size = 14)) +
+  labs(title = "Comparison Graph", subtitle = paste("Samples ", samples[1]," vs ", samples[2]))
+plot_data
